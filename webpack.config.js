baae08484735d75107b56f8dcebca3af3117167f
strangedk <strangedik@gmail.com>
2020-07-08 14:30:14 +0300

    gitignore fix

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,6 @@ module.exports = {
     },
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "dista"),
+        path: path.resolve(__dirname, "dist"),
     },
 };
